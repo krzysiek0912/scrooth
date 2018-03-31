ea112b68a9854e1f98f6c9bb59e6998f1302f1ee
Rafał Spiżewski <rafal.spizewski@gmail.com>
2018-03-31 21:04:22 +0200

    Initial commit

---
+++ src/js/scrooth.js
@@ -0,0 +1,47 @@
+class Scrooth {
+  constructor(options) {
+    const config = {...options}
+    this.element = config.element || window;
+    this.distance = config.strength || 10;
+    this.acceleration = config.acceleration || 1.5;
+    this.deceleration = config.deceleration || 0.975;
+
+    this.running = false;
+
+    this.element.addEventListener('onwheel', this.scrollHandler.bind(this));
+    this.element.addEventListener('DOMMouseScroll', this.scrollHandler.bind(this));
+    this.element.addEventListener('mousewheel', this.scrollHandler.bind(this));
+
+    this.scroll = this.scroll.bind(this);
+  }
+
+  scrollHandler(e) {
+    e.preventDefault();
+
+    if (this.running === false) {
+      this.running = true;
+      this.currentDistance = e.deltaY > 0 ? 1 : -1;
+      this.isDistanceAsc = true;
+      this.scroll();
+    } else {
+      this.isDistanceAsc = false;
+      this.currentDistance = e.deltaY > 0 ? this.distance : -this.distance;
+    }
+  }
+
+  scroll() {
+    if (this.running === true) {
+      requestAnimationFrame(this.scroll);
+      let top = this.element.pageYOffset || this.element.scrollTop;
+      top = typeof top === 'undefined' ? 0 : top;
+
+      this.element.scrollTo(0, top + this.currentDistance);
+
+      this.currentDistance *= this.isDistanceAsc === true ? this.acceleration : this.deceleration;
+      Math.abs(this.currentDistance) >= Math.abs(this.distance) ? this.isDistanceAsc = false : 1;
+      Math.abs(this.currentDistance) < 1 && this.isDistanceAsc === false ? this.running = false : 1 ;
+    }
+  }
+}
+
+const body = new Scrooth();
